7e073ebfd248c97b662adc9f85e5ad0f5680c9b1
Steven127 <1270728683@qq.com>
2022-03-01 17:48:05 +0800

    add: oss存储和md5工具类

--- server/config/config.go
+++ server/config/config.go
@@ -9,4 +9,11 @@ type Server struct {
 	Zap Zap `mapstructure:"zap" json:"zap" yaml:"zap"`
 	// 鉴权
 	JWT JWT `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
+	// oss
+	Local      Local      `mapstructure:"local" json:"local" yaml:"local"`
+	Qiniu      Qiniu      `mapstructure:"qiniu" json:"qiniu" yaml:"qiniu"`
+	AliyunOSS  AliyunOSS  `mapstructure:"aliyun-oss" json:"aliyunOSS" yaml:"aliyun-oss"`
+	HuaWeiObs  HuaWeiObs  `mapstructure:"hua-wei-obs" json:"huaWeiObs" yaml:"hua-wei-obs"`
+	TencentCOS TencentCOS `mapstructure:"tencent-cos" json:"tencentCOS" yaml:"tencent-cos"`
+	AwsS3      AwsS3      `mapstructure:"aws-s3" json:"awsS3" yaml:"aws-s3"`
 }
